0f32157987a36c97252c9a1991a45fc514613c51
minhkhoidoan <minhkhoidoan234@gmail.com>
2022-10-20 10:05:58 +0700

    structure project

--- practice/src/main.jsx
+++ practice/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import GlobalStyles from '././components/GlobalStyles/index.js';
+import GlobalStyles from './theme/GlobalStyles/index.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
