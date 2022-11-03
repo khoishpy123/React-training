f3f68ccaceab2e7732d6cc85b7bd0c37c7ddd59f
minhkhoidoan <minhkhoidoan234@gmail.com>
2022-11-03 22:41:06 +0700

    move folder Ellipsis into component button

--- practice/src/components/Button/Ellipsis/Ellipsis.jsx
+++ practice/src/components/Button/Ellipsis/Ellipsis.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Icon } from '@iconify/react';
-import DropDownContent from '../DropDownItem/DropDownItem';
+import DropDownContent from '../../DropDownItem/DropDownItem';
 
 import styles from './DropDown.module.scss';
 
