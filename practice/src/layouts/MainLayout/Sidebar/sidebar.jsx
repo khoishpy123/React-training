ab5924921e75ddf7dcece69b97c18a3e9eb05e9b
minhkhoidoan <minhkhoidoan234@gmail.com>
2022-11-14 13:03:43 +0700

    fix css header and sidebar

--- practice/src/layouts/MainLayout/Sidebar/sidebar.jsx
+++ practice/src/layouts/MainLayout/Sidebar/sidebar.jsx
@@ -7,23 +7,31 @@ import config from '../../.././config/index';
 function Sidebar() {
   return (
     <aside className={styles.wrapper}>
-      <nav className={styles.nav_sidebar}>
-        <MenuItem
-          title="Dashboard"
-          to={config.routes.dashboard}
-          icon={<Icon icon="ant-design:line-chart-outlined" />}
-        />
-        <MenuItem
-          title="Student"
-          to={config.routes.user}
-          icon={<Icon icon="bxs:user-rectangle" />}
-        />
-        <MenuItem
-          title="Not Found"
-          to={config.routes.notfound}
-          icon={<Icon icon="fe:disabled" />}
-        />
-      </nav>
+      <div className={styles.box_content}>
+        <div className={styles.box_avatar}>
+          <div className={styles.avatar_image}></div>
+          <div></div>
+        </div>
+      </div>
+      <div>
+        <nav className={styles.nav_sidebar}>
+          <MenuItem
+            title="Dashboard"
+            to={config.routes.dashboard}
+            icon={<Icon icon="ant-design:line-chart-outlined" />}
+          />
+          <MenuItem
+            title="Student"
+            to={config.routes.user}
+            icon={<Icon icon="bxs:user-rectangle" />}
+          />
+          <MenuItem
+            title="Not Found"
+            to={config.routes.notfound}
+            icon={<Icon icon="fe:disabled" />}
+          />
+        </nav>
+      </div>
     </aside>
   );
 }
